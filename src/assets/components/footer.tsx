f790ea2b6327100a178c70311268df0bbb1d5bf8
Michael Anthony Tura <turamichael166@gmail.com>
2026-07-31 12:47:25 +0800

    second commit

--- src/assets/components/footer.tsx
+++ src/assets/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer(){
-  <>
-  
+  return(
+
      <section className="w-full bg-black py-16 px-8 md:px-16 text-white ">
           <h1 className="font-bold text-2xl text-center mb-10">LITERACY</h1>
           <p className="text-center mb-5">888 Griffiths Way, Mainland ML 12345</p>
@@ -8,14 +8,9 @@ export function Footer(){
                     <p className="text-center mb-10">info@yoursite.com</p>
                     <p className="text-center mb-20">Copyright 2022 Literacy Blog. All Rights Reserved.</p>
                      <p className="text-center font-bold text-2xl mb-5">W</p>
-                     <p className="text-center">Website Created & Hosted with Website.com Website Builder</p>
-
-                    
-
-
+                     <p className="text-center">Website Created & Hosted with Website.com Website Builder</p>     
 
         </section>
-      
+  ); 
   
-  </>
 }
--- src/assets/pages/home.tsx
+++ src/assets/pages/home.tsx
@@ -78,21 +78,6 @@ export default function HomePage() {
            <div className="w-full h-[280px] bg-cover bg-center" style={{ backgroundImage: `url(${D})`}}></div>
             </div>
         </section>
-
-        <section className="w-full bg-black py-16 px-8 md:px-16 text-white ">
-          <h1 className="font-bold text-2xl text-center mb-10">LITERACY</h1>
-          <p className="text-center mb-5">888 Griffiths Way, Mainland ML 12345</p>
-                    <p className="text-center mb-5">(987) 654-3210</p>
-                    <p className="text-center mb-10">info@yoursite.com</p>
-                    <p className="text-center mb-20">Copyright 2022 Literacy Blog. All Rights Reserved.</p>
-                     <p className="text-center font-bold text-2xl mb-5">W</p>
-                     <p className="text-center">Website Created & Hosted with Website.com Website Builder</p>
-
-                    
-
-
-
-        </section>
       
       <Footer />
     </>
